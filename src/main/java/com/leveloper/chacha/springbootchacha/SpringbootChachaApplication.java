0974c1d91d60c70f3cc4f8bc413c50827d9f80e8
tkdgusl94@gmail.com <tkdgusl94@gmail.com>
2020-01-19 20:06:57 +0900

    project init

---
+++ src/main/java/com/leveloper/chacha/springbootchacha/SpringbootChachaApplication.java
@@ -0,0 +1,13 @@
+package com.leveloper.chacha.springbootchacha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootChachaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootChachaApplication.class, args);
+	}
+
+}
