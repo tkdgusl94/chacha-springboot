c21ae75fa84b33f642d249d8f0f5489fc464b148
tkdgusl94 <tkdgusl94@gmail.com>
2020-02-01 01:10:10 +0900

    마지막 과연?

--- src/main/resources/db/realdb/schema.sql
+++ src/main/resources/db/realdb/schema.sql
@@ -1,7 +1,7 @@
-CREATE TABLE test(
+CREATE TABLE IF NOT EXISTS test(
     id bigint not null primary key,
     name varchar(255)
 ) engine=InnoDB;
 
-insert into test values (1, 'name');
+insert into test values (2, 'name');
 INSERT INTO user values (3, '2020-01-31 22:10:44.712903', '2020-01-31 22:10:44.712903', 'tkdgusl94@gmail.com', 'jeong', 'picture');
