06c85d203cf03e8c7af667009818306bc709790e
tkdgusl94 <tkdgusl94@gmail.com>
2020-02-01 00:31:20 +0900

    application-real-db에서 스키마 선언했음

--- src/main/resources/db/realdb/schema.sql
+++ src/main/resources/db/realdb/schema.sql
@@ -1,6 +1,7 @@
-INSERT INTO user values (3, '2020-01-31 22:10:44.712903', '2020-01-31 22:10:44.712903', 'tkdgusl94@gmail.com', 'jeong', 'picture');
-
 CREATE TABLE test(
     id bigint not null primary key,
     name varchar(255)
 ) engine=InnoDB;
+
+insert into test values (1, 'name');
+INSERT INTO user values (3, '2020-01-31 22:10:44.712903', '2020-01-31 22:10:44.712903', 'tkdgusl94@gmail.com', 'jeong', 'picture');
