225bf5de26781ee097ff4fd495b2a95b33dcff6a
tkdgusl94 <tkdgusl94@gmail.com>
2020-02-01 00:00:21 +0900

    오타 수정

--- src/main/resources/db/realdb/schema.sql
+++ src/main/resources/db/realdb/schema.sql
@@ -1,6 +1,6 @@
 INSERT INTO user values (3, '2020-01-31 22:10:44.712903', '2020-01-31 22:10:44.712903', 'tkdgusl94@gmail.com', 'jeong', 'picture');
 
 CREATE TABLE test(
-    id not null primary key,
-    name
+    id bigint not null primary key,
+    name varchar(255)
 ) engine=InnoDB;
