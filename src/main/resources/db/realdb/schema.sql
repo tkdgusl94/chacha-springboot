718497c3de25e0d69ab6e5acfb7b991870876589
tkdgusl94 <tkdgusl94@gmail.com>
2020-02-01 01:36:50 +0900

    이제 자자

--- src/main/resources/db/realdb/schema.sql
+++ src/main/resources/db/realdb/schema.sql
@@ -3,5 +3,5 @@ CREATE TABLE IF NOT EXISTS test(
     name varchar(255)
 ) engine=InnoDB;
 
-insert into test values (2, 'name');
-INSERT INTO user values (3, '2020-01-31 22:10:44.712903', '2020-01-31 22:10:44.712903', 'tkdgusl94@gmail.com', 'jeong', 'picture');
+insert into test values (3, 'name');
+INSERT INTO user values (4, '2020-01-31 22:10:44.712903', '2020-01-31 22:10:44.712903', 'tkdgusl94@gmail.com', 'jeong', 'picture');
